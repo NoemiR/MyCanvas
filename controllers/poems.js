e3b405373ba4a78fb4a8b2a0aa37e813f684e811
Noemi Romero <nlll02@yahoo.com>
2018-04-19 16:44:27 -0500

    setup the controllers and server files

---
+++ controllers/poems.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const Photos = require('../models/photo');
+const Users = require('../models/user');
+const Poems = require('../models/poem');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
