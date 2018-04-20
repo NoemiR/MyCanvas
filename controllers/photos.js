274fe026ed77851849b050a0a66018791e12d7ae
Hannah Werman <hannah.werman@gmail.com>
2018-04-20 11:20:19 -0500

    created "new" view to add user content

--- controllers/photos.js
+++ controllers/photos.js
@@ -4,8 +4,22 @@ const Photos = require('../models/photo');
 const Users = require('../models/user');
 const Poems = require('../models/poem');
 
+// writing this as a content controller until it no longer makes sense to do so
+// will be able to manipulate photos and poetry, hopefully will let us avoid writing two controllers that do the same thing on the same ejs page
 
+// ** new ** content route
+router.get('/new', (req, res, next) => {
+	res.render('photos/new.ejs')
+})
 
+// ** edit ** content route
+// router.get('/:id/edit', async (req, res, next) => {
+// 	try {
+
+// 	}catch(err) {
+// 		next(err);
+// 	}
+// })
 
 
 
