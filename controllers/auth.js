c5d8f5af0316c765770e2c757b743ab45b510588
Noemi Romero <nlll02@yahoo.com>
2018-04-23 10:50:38 -0500

    created session and password requirements

---
+++ controllers/auth.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user')
+const bcrypt = require('bcrypt');
+
+router.get('/', (req, res) => {
+	res.render('users/login')	
+})
+
+route.post('users/login', async (req, res, next) => {
+	try {
+		const user = await User.findOne({username: req.body.username})
+		if(user){
+			if(bcrypt.compareSync(req.body.password, user.password)){
+				req.session.logged = true;
+				req.session.username = user.username;
+				res.redirect('/')
+			}
+		} else {
+			req.session.message = "username or password is incorrect"
+			res.redirect('/')
+		}
+
+	}catch (err) {
+		next(err)
+	}
+})
+
+router.post('users/new', async (req, res, next) => {
+	const password = req.body.password;
+	const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+	const newUser = {
+		username: req.body.username,
+		password: passwordHash,
+		email: req.body.email
+	};
+	try {
+		const user = await User.create(newUser)
+		if(user){
+			req.session.logged = true;
+			req.session.username = user.username;
+			res.redirect('users/index.ejs')
+		} else {
+			req.session.message = 'Sorry try again'
+			res.redirect('users/login')
+		}
+
+	} catch (err) {
+		next(err)
+	}	
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
