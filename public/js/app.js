44e7b155f3c527a52b8c55edaae5616addd8fbe7
Noemi Romero <nlll02@yahoo.com>
2018-04-25 18:32:16 -0500

    updated navbar and positioned on the top of pages

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+
+
+const navbar = document.getElementById("navbar");
+const sticky = navbar.offsetTop;
+
+function stickyNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+window.onscroll = function() {stickyNav()};
